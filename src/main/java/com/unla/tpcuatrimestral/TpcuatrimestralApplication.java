462e805d018d3d795073463078b689b9c8a1329b
mjaldinmontero <micaelajmontero@hotmail.com>
2020-05-16 14:10:09 -0300

    adding spring files

---
+++ src/main/java/com/unla/tpcuatrimestral/TpcuatrimestralApplication.java
@@ -0,0 +1,13 @@
+package com.unla.tpcuatrimestral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpcuatrimestralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpcuatrimestralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unla/tpcuatrimestral/TpcuatrimestralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.tpcuatrimestral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpcuatrimestralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
